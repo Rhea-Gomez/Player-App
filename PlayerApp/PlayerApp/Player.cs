72c719e7ff631b2d794158fa18e2992808d30753
Rhea-Gomez <rhea.gomez@aurionpro.com>
2025-07-22 14:06:56 +0530

    Player App assignment

---
+++ PlayerApp/PlayerApp/Player.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerApp
+{
+    public class Player
+    {
+        private readonly int id;
+        private readonly string name;
+        private readonly int age;
+
+        public Player(int id, string name, int age)
+        {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+        }
+
+        public Player(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+            this.age = 0;
+        }
+
+        public int GetId()
+        {
+            return id;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public int GetAge()
+        {
+            return age;
+        }
+
+        public Player WhoisElder(Player player)
+        {
+            if (this.age > player.GetAge())
+            {
+                return this;
+            }
+            else
+            {
+                return player;
+            }
+        }
+    }
+}
---
+++ PlayerApp/PlayerApp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace PlayerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player p1 = new Player(101, "Rhea", 23);
+            Player p2 = new Player(102, "Sanjana", 22);
+
+            Player elder = p1.WhoisElder(p2);
+            Console.WriteLine($"Elder Player: {elder.GetName()}, Age: {elder.GetAge()}");
+        }
+    }
+}
